938f241d9f9157fcc59e2019c618317c4f6a912d
Ian Saxon <isaxon@oxfordeconomics.com>
2018-09-06 10:11:05 +0100

    Add schengen list

---
+++ schengen.py
@@ -0,0 +1,34 @@
+# Get UN country list
+
+# Install dependencies
+# pip install requests
+# pip install BeautifulSoup4
+
+import urllib.request
+from bs4 import BeautifulSoup
+import datetime
+import datetime
+
+page = urllib.request.urlopen('https://europa.eu/european-union/about-eu/countries_en')
+
+soup = BeautifulSoup(page,'html.parser')
+
+soup = soup.find('div',id='sub-section-1')
+
+soup = soup.find_all('li')
+
+print(soup)
+
+# Initialise new csv file
+f=open('CountryList-Schengen.csv','w')
+f.write('SchengenMemberStates'+datetime.datetime.today().strftime('%Y-%m-%d')+'\n')
+f.close()
+
+# Add countries
+for x in soup:
+	soup = x.text
+	print(soup)
+	f=open('CountryList-Schengen.csv','a')
+	f.write(soup+'\n')
+	f.close()
+
