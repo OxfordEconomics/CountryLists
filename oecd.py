9b8a9fa2ad2ed6f99d824b55af8b67850fdd65c5
Ian Saxon <isaxon@oxfordeconomics.com>
2018-09-06 11:47:07 +0100

    Add OECD member countries

---
+++ oecd.py
@@ -0,0 +1,38 @@
+# Get UN country list
+
+# Install dependencies
+# pip install requests
+# pip install BeautifulSoup4
+
+import urllib.request
+from bs4 import BeautifulSoup
+import datetime
+import datetime
+
+page = urllib.request.urlopen('http://www.oecd.org/about/membersandpartners/list-oecd-member-countries.htm')
+
+soup = BeautifulSoup(page,'html.parser')
+
+
+
+soup = soup.find('table',align='center')
+
+soup = soup.find_all('a')
+
+print (soup)
+
+
+# Initialise new csv file
+f=open('CountryList-OECD.csv','w')
+f.write('OecdMembers'+datetime.datetime.today().strftime('%Y-%m-%d')+'\n')
+f.close()
+
+# Add countries
+for x in soup:
+	if x.text != 'More on membership and enlargement':
+		soup = x.text.title() #use title() to capitalise the beginning of each part of country names
+		print(soup)
+		f=open('CountryList-OECD.csv','a')
+		f.write(soup+'\n')
+		f.close()
+	
