6f4a0cab1ef8c1e9a3c2b2393476325487a6aa27
Ian Saxon <isaxon@oxfordeconomics.com>
2019-03-05 11:07:08 +0000

    Add Mercosur member states

---
+++ updateCountryLists/mercosur.py
@@ -0,0 +1,43 @@
+# Get UN country list
+
+# Install dependencies
+# pip install requests
+# pip install BeautifulSoup4
+
+import urllib.request
+from bs4 import BeautifulSoup
+import datetime
+
+page = urllib.request.urlopen('https://www.mercosur.int/en/about-mercosur/mercosur-countries/')
+
+soup = BeautifulSoup(page,'html.parser')
+
+print(soup)
+
+memberstates = soup.find_all('h3')
+
+
+# Replace the cruft
+memberstates = str(memberstates).replace("<h3 style=\"text-align: center;\">","").replace("</h3>","").replace("<span style=\"color: #0055b7;\">","").replace("</span>","").replace("<span style=\"color: #ffffff;\">","").replace("<br/>","").replace(" ,",",").replace(" Entry of new member States","").replace("*","")
+
+# Add newline after every comma
+memberstates = memberstates.replace(", ","\n")
+
+# Add newline before and after headings
+memberstates = memberstates.replace("Associated States","\n"+"# ASSOCIATED STATES"+"\n")
+memberstates = memberstates.replace("States Parties","\n"+"# STATES PARTIES"+"\n")
+
+# Add date to beginning of file and remove brackets from end of file
+memberstates = memberstates.replace("[","CountryName"+datetime.datetime.today().strftime('%Y-%m-%d')+"\n").replace(",]","")
+
+
+f = open('../countryList-mercosur.csv','w')
+f.write(memberstates)
+f.close()
+
+f = open('../countryList-mercosur.csv','r')
+print(f.readlines())
+
+
+
+
